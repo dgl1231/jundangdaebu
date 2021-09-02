fc139b5da0c0a00d7de0b61d3aca07604d7413ad
dgl1231 <63222048+dgl1231@users.noreply.github.com>
2021-09-02 17:30:11 +0900

    layout

--- public/js/map.js
+++ public/js/map.js
@@ -1,8 +1,9 @@
+
 var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
-    mapOption = { 
-        center: new kakao.maps.LatLng(37.51858357624814, 127.02802309113164), // 지도의 중심좌표
-        level: 6 // 지도의 확대 레벨
-    };
+mapOption = { 
+    center: new kakao.maps.LatLng(37.51858357624814, 127.02802309113164), // 지도의 중심좌표
+    level: 6 // 지도의 확대 레벨
+};
 
 var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
 
@@ -11,11 +12,8 @@ var markerPosition  = new kakao.maps.LatLng(37.51858357624814, 127.0280230911316
 
 // 마커를 생성합니다
 var marker = new kakao.maps.Marker({
-    position: markerPosition
+position: markerPosition
 });
 
 // 마커가 지도 위에 표시되도록 설정합니다
 marker.setMap(map);
-
-// 아래 코드는 지도 위의 마커를 제거하는 코드입니다
-// marker.setMap(null);    
