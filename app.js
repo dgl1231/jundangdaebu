81dd4e65280e48f9695bc7fe8d65cfa2c3c94e9f
dgl12312 <dgl1231@naver.com>
2021-11-11 14:45:26 +0900

    app.js

--- app.js
+++ app.js
@@ -23,13 +23,14 @@ app.set('layout', 'layout/layout');
 
 //listening
 app.listen(PORT, () => console.log('Listening on http://localhost:${ ' + PORT + ' }'))
-
+var loginsession = 0;
 
 express().use(require('express-ejs-layouts'))
 //route randering
 //home
 app.get('/', (req, res) => {
     app.locals.styleNo = 0;
+    app.locals.login = loginsession;
     res.render('index.ejs', {
         title: "HOME | " + siteData.title,
         address: siteData.address
@@ -39,6 +40,7 @@ app.get('/', (req, res) => {
 //대출받기;
 app.get('/loan', (req, res) => {
     app.locals.styleNo = 1;
+    app.locals.login = loginsession;
     res.render(__dirname + '/views/loan.ejs', {
         title: "대출받기 | " + siteData.title
     });
@@ -46,6 +48,7 @@ app.get('/loan', (req, res) => {
 //택배
 app.get('/post', (req, res) => {
     app.locals.styleNo = 2;
+    app.locals.login = loginsession;
     res.render(__dirname + '/views/post.ejs', {
         title: "배송안내 | " + siteData.title
         
@@ -54,6 +57,7 @@ app.get('/post', (req, res) => {
 //한도문의
 app.get('/dambo', (req, res) => {
     app.locals.styleNo = 3;
+    app.locals.login = loginsession;
     res.render(__dirname + '/views/dambo.ejs', {
         title: "한도문의 | " + siteData.title
     });
@@ -61,6 +65,7 @@ app.get('/dambo', (req, res) => {
 //대출이력
 app.get('/dambolist', (req, res) => {
     app.locals.styleNo = 4;
+    app.locals.login = loginsession;
     res.render(__dirname + '/views/dambolist.ejs', {
         title: "대출이력 | " + siteData.title
         
@@ -69,6 +74,7 @@ app.get('/dambolist', (req, res) => {
 //마이페이지
 app.get('/mypage', (req, res) => {
     app.locals.styleNo = 5;
+    app.locals.login = loginsession;
     res.render(__dirname + '/views/mypage.ejs', {
         title: "마이페이지 | " + siteData.title
         
@@ -77,6 +83,7 @@ app.get('/mypage', (req, res) => {
 //회원가입
 app.get('/sign_up', (req, res) => {
     app.locals.styleNo = 6;
+    app.locals.login = loginsession;
     res.render(__dirname + '/views/signup.ejs', {
         title: "로그인 | " + siteData.title
     });
